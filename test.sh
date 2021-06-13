86e3d26c575d9c837433f2255b786ac507b736e1
Anita Chang <Huilong494@gmail.com>
2021-06-12 18:23:55 -0700

    Test utils and utility script

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -X POST -H "Content-Type: application/json" -d '{"value":"Dev1"}' http://localhost:8080/almas
+curl -X POST -H "Content-Type: application/json" -d '{"value":"Developer2"}' http://localhost:8080/anita
